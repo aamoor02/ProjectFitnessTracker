13fcd5fd778458dbaf37d4c1f960ffd9f8d37c6d
Aaron A Moorman <amoorman01@gmail.com>
2020-11-16 17:18:12 -0500

    Add conversion functions
    Additional functions added to help with conversions

--- FitnessFun.py
+++ FitnessFun.py
@@ -30,3 +30,22 @@ birthday = check_birthday()
 height = input('what is your height in inches?\n')
 weight = input('what is your weight in pounds?\n')
 email = check_email()
+
+def convert_to_kilogram(user_weight):
+    ''' convert user provided weight into kilograms '''
+    converted_weight = float(user_weight) * 0.453592
+    new_weight = int(converted_weight)
+    return new_weight
+
+def convert_to_meters(user_height):
+    ''' convert user provided height into meters '''
+    converted_height = float(user_height) * .0254
+    return converted_height
+
+
+def print_info():
+    print("Your name is {} {}".format(first_name, last_name))
+    print("Your birthday is {}-{}-{}".format(birthday.month,birthday.day,birthday.year))
+    print("your weight in kilograms is {}.".format(convert_to_kilogram(weight)))
+    print("Your height in meters is {}".format(convert_to_meters(height)))
+    print("Your email address is {}.".format(email))
